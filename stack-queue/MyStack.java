0c08850ba6e098b3d6eed4794deb6ca3770e4693
Kuruva Pavani <pavanikuruva2109@gmail.com>
2025-12-02 16:04:50 +0530

    Implement Stack using Queue

---
+++ stack-queue/MyStack.java
@@ -0,0 +1,33 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MyStack {
+
+    private Queue<Integer> q;
+
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        q.add(x);
+        int size = q.size();
+        for (int i = 0; i < size - 1; i++) {
+            q.add(q.peek());
+            q.remove();
+        }
+    }
+
+    public int pop() {
+        return q.remove();
+    }
+
+    public int top() {
+        return q.peek();
+    }
+
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
