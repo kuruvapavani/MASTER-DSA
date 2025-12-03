f949619e12e855dbe756c0029bbdfb2b287d819e
Kuruva Pavani <pavanikuruva2109@gmail.com>
2025-12-03 12:11:42 +0530

    Solve Check if Tree Is Balanced

---
+++ trees/IsBalanced.java
@@ -0,0 +1,20 @@
+public class IsBalanced {
+
+    public static boolean isBalanced(TreeNode root) {
+        return check(root) != -1;
+    }
+
+    private static int check(TreeNode node) {
+        if (node == null) return 0;
+
+        int left = check(node.left);
+        if (left == -1) return -1;
+
+        int right = check(node.right);
+        if (right == -1) return -1;
+
+        if (Math.abs(left - right) > 1) return -1;
+
+        return 1 + Math.max(left, right);
+    }
+}
