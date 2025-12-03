4acf9d54de1f1fe3016032d8e7d6457dbf6800b6
Kuruva Pavani <pavanikuruva2109@gmail.com>
2025-12-03 08:26:40 +0530

    Implement tree traversals: preorder, inorder, postorder

---
+++ trees/Traversals.java
@@ -0,0 +1,23 @@
+public class Traversals {
+
+    public static void preorder(TreeNode root) {
+        if (root == null) return;
+        System.out.print(root.val + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void inorder(TreeNode root) {
+        if (root == null) return;
+        inorder(root.left);
+        System.out.print(root.val + " ");
+        inorder(root.right);
+    }
+
+    public static void postorder(TreeNode root) {
+        if (root == null) return;
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.val + " ");
+    }
+}
