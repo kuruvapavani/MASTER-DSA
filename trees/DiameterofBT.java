a870f77bc25ce68df5c58f617fa65cdb45ba114d
Kuruva Pavani <pavanikuruva2109@gmail.com>
2025-12-03 12:15:10 +0530

    Solve Find diameter of binary tree

---
+++ trees/DiameterofBT.java
@@ -0,0 +1,24 @@
+
+public class DiameterofBT {
+
+    static int maxDiameter = 0;
+
+    public static int diameterOfBinaryTree(TreeNode root) {
+        maxDiameter = 0;
+        height(root);
+        return maxDiameter;
+    }
+
+    private static int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int left = height(node.left);
+        int right = height(node.right);
+
+        maxDiameter = Math.max(maxDiameter, left + right);
+
+        return 1 + Math.max(left, right);
+    }
+}
