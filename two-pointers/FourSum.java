208a51363ad1a82577a52927b566578900c2c5ab
Kuruva Pavani <pavanikuruva2109@gmail.com>
2025-12-20 18:25:22 +0530

    Solve 4Sum using sorting and two pointers with duplicate handling

---
+++ two-pointers/FourSum.java
@@ -0,0 +1,44 @@
+
+import java.util.*;
+
+public class FourSum {
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < nums.length - 3; i++) {
+            int a = nums[i];
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int b = nums[j];
+                int left = j + 1;
+                int right = nums.length - 1;
+                while (left < right) {
+                    long sum = (long) a + b + nums[left] + nums[right];
+                    if (sum == target) {
+                        List<Integer> list = Arrays.asList(a, b, nums[left], nums[right]);
+                        ans.add(list);
+                        left++;
+                        right--;
+                        while (left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+                    } else if (sum > target) {
+                        right--;
+                    } else {
+                        left++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
