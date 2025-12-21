a4681453c8ef0863c906689de7cc97c88a84ec32
Kuruva Pavani <pavanikuruva2109@gmail.com>
2025-12-21 13:56:14 +0530

    LC 41 First Missing Positive using in-place indexing

---
+++ arrays/FirstMissingPositive.java
@@ -0,0 +1,29 @@
+
+public class FirstMissingPositive {
+
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= 0) {
+                nums[i] = n + 1;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int num = Math.abs(nums[i]);
+            if (num > 0 && num <= n) {
+                if (nums[num - 1] > 0) {
+                    nums[num - 1] *= -1;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+
+}
