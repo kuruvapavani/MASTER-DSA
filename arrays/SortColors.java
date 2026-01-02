daca2033ae0a41c44e1cac541a590a89c5e74682
Kuruva Pavani <pavanikuruva2109@gmail.com>
2026-01-02 12:28:26 +0530

    LC 75 : Sort Colors using Dutch National Flag (one pass)

---
+++ arrays/SortColors.java
@@ -0,0 +1,25 @@
+
+public class SortColors {
+
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(low, mid, nums);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(high, mid, nums);
+                high--;
+            }
+        }
+    }
+
+    private void swap(int i, int j, int[] arr) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
