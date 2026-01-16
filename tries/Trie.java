439983f6d0946d9ce74b72f0add52ada821e98a7
Kuruva Pavani <pavanikuruva2109@gmail.com>
2026-01-16 18:39:16 +0530

    Implementation of Trie

---
+++ tries/Trie.java
@@ -0,0 +1,51 @@
+
+public class Trie {
+
+    class TrieNode {
+
+        TrieNode[] children = new TrieNode[26];
+        boolean isEnd;
+    }
+
+    TrieNode root;
+
+    public void Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char ch : word.toCharArray()) {
+            int idx = ch - 'a';
+            if (node.children[idx] == null) {
+                node.children[idx] = new TrieNode();
+            }
+            node = node.children[idx];
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char ch : word.toCharArray()) {
+            int idx = ch - 'a';
+            if (node.children[idx] == null) {
+                return false;
+            }
+            node = node.children[idx];
+        }
+        return node.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (char ch : prefix.toCharArray()) {
+            int idx = ch - 'a';
+            if (node.children[idx] == null) {
+                return false;
+            }
+            node = node.children[idx];
+        }
+        return true;
+    }
+}
